7af4b867fbc7b88ebff8e8d4b4bf0f2e64d746f0
Best-code <cpmraygun@gmail.com>
2024-12-05 15:19:34 -0500

    2025 Repost Update

--- sc_unrepost.user.js
+++ sc_unrepost.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         sc_unrepost
-// @namespace    https://github.com/teuncm
+// @namespace    https://github.com/Best-code
 // @author       Teun M.
 // @version      1.1
 // @match        https://soundcloud.com/*/reposts
@@ -10,7 +10,7 @@
 /* Number of deletions. N = -1 will delete everything. */
 var N = -1;
 /* Time interval in milliseconds between each delete. */
-var interval = 125;
+var interval = 50;
 
 var $ = window.jQuery;
 $(window).on("load", function() {
@@ -19,11 +19,16 @@ $(window).on("load", function() {
 });
 
 function performAction() {
-    var button = $("button[title='Unpost']").first();
+    var button = $("button[title='Unpost / Edit Repost']").first();
 
     if(null != button) {
         if(N-- != 0) {
             button.click();
+            var delButton = $("button[class='sc-button sc-button-nostyle sc-button-medium repostOverlay__formButton repostOverlay__formButtonDelete']").first();
+            if(null != delButton) {
+                delButton.click();
+            }
+
         } else {
             return;
         }
